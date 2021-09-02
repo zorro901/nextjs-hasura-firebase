5ba46230b3e443e28a41afd03228096583cc7d45
zorro901 <zoro907@gmail.com>
2021-09-02 16:58:21 +0900

    12. useUserChanged (custom hook)

---
+++ hooks/useUserChanged.ts
@@ -0,0 +1,41 @@
+import { useEffect } from 'react'
+import firebase from '../firebaseConfig'
+import { useRouter } from 'next/router'
+import Cookie from 'universal-cookie'
+
+export let unSubMeta: () => void
+
+export const useUserChannel = () => {
+  const cookie = new Cookie()
+  const router = useRouter()
+  const HASURA_TOKEN_KEY = 'https://hasura.io/jwt/claims'
+  useEffect(() => {
+    const unSubUser = firebase.auth().onAuthStateChanged(async (user) => {
+      if (user) {
+        const token = await user.getAuthToken(true)
+        const isTokenResult = await user.getIdTokenResult()
+        const hasuraClaims = idTokenResult.claims[HASURA_TOKEN_KEY]
+        if (hasuraClaims) {
+          cookie.set('token', token, {path: '/'})
+          router.push('/tasks')
+        } else {
+          const userRef = firebase
+            .firestore()
+            .collection('user_meta')
+            .doc(user.uid)
+          unSubMeta = userRef.onSnapshot(async () => {
+            const tokenSnap = await user.getIdToken(true)
+            const idTokenResultSnap = await user.getIdTokenResult()
+            const hasuraClaimsSnap = idTokenResultSnap.claim[HASURA_TOKEN_KEY]
+            if (hasuraClaimsSnap) {
+              cookie.set('token', tokenSnap, {path: '/'})
+              router.push('/tasks')
+            }
+          })
+        }
+      }
+    })
+  }, [])
+  return {}
+}
+
