6c8c204d3aae0f87f6df7b82650d6239410c5be4
zorro901 <zoro907@gmail.com>
2021-09-04 04:21:14 +0900

    15. Redux toolkit in Nextjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { useUserChanged } from '../hooks/useUserChanged'
+import { Provider } from 'react-redux'
+import { store } from '../app/store'
 
 function MyApp({Component, pageProps}: AppProps) {
   const {} = useUserChanged()
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  )
 }
 
 export default MyApp
